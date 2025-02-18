81625eb0b88e2b2496c5d09e80c782573b923595
Ines Sainz Ramon <isainz-r@c1r10s4.42madrid.com>
2025-02-18 17:31:29 +0100

    ex02 not done

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isainz-r <isainz-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 17:22:47 by isainz-r          #+#    #+#             */
+/*   Updated: 2025/02/18 17:22:52 by isainz-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <algorithm>
+
+class MutantStack
+{
+private:
+	/* data */
+public:
+	MutantStack( void );
+	~MutantStack();
+};
+
+MutantStack::MutantStack( void )
+{
+}
+
+MutantStack::~MutantStack()
+{
+}
+
