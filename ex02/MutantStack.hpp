1f0fc637a0887206395875be2f76db4bc510143c
Ines Sainz Ramon <isainz-r@c1r11s2.42madrid.com>
2025-02-19 12:42:44 +0100

    ex02 maybe

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -13,21 +13,40 @@
 #pragma once
 #include <iostream>
 #include <algorithm>
+#include <stack>
 
-class MutantStack
+template<class T>
+class MutantStack : public std::stack<T>
 {
-private:
-	/* data */
-public:
-	MutantStack( void );
-	~MutantStack();
+	private:
+		/* data */
+
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		iterator begin()
+		{
+			return (this->c.begin());
+		}
+		iterator end()
+		{
+			return (this->c.end());
+		}
+
+		const_iterator begin() const
+		{
+			return (this->c.begin());
+		}
+		const_iterator end() const
+		{
+			return (this->c.end());
+		}
+
+		MutantStack& operator=( const MutantStack& before );
+		MutantStack( const MutantStack& before );
+		MutantStack( void );
+		~MutantStack();
 };
 
-MutantStack::MutantStack( void )
-{
-}
-
-MutantStack::~MutantStack()
-{
-}
-
+#include "MutantStack.tpp"
